81ebfcd7756573299002721abc5c640b7d6e9c43
us-shah01 <shahudit2301@gmail.com>
2026-02-20 10:20:49 +0530

    Add files via upload

---
+++ ath_bank_detail/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'IFSC Code and Branch for Bank Account',
+    'version': "18.0.1.0.0",
+    'category': 'Sales/CRM',
+    'description': "IFSC Code and Branch for Bank Account",
+    'author': "Ath Software Solutions Pvt. Ltd.",
+    'depends': ['base', 'mail'],
+    'data': [
+        'views/bank_ifsc_branch.xml',
+    ],
+    'license': 'OPL-1',
+    'installable': True,
+    'application': True,
+}
