f7bfe0789f7f156a857daa143789171063896836
us-shah01 <shahudit2301@gmail.com>
2026-02-20 10:24:19 +0530

    Update __manifest__.py

--- ath_bank_detail/__manifest__.py
+++ ath_bank_detail/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'IFSC Code and Branch for Bank Account',
-    'version': "19.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Sales/CRM',
     'description': "IFSC Code and Branch for Bank Account",
     'author': "Ath Software Solutions Pvt. Ltd.",
@@ -12,4 +12,5 @@
     'installable': True,
     'application': True,
 }
+
 
